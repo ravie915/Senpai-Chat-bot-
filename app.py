85115077252aa2996e7759037103f99ff7b93b26
ravie915 <134964669+ravie915@users.noreply.github.com>
2026-03-15 18:13:56 +0200

    Update app.py

--- app.py
+++ app.py
@@ -182,7 +182,7 @@ section[data-testid="stSidebar"],
     .logo {{ 
         width: 60px !important; 
         position: relative !important; 
-        top: 20px !important; 
+        top: 0px !important; 
     }}
 
     .senpai-title {{
